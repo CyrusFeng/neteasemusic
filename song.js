5c1a35db55a978cb77545d4e44d2e6b9bc8cb014
Elemento <18733660998@163.com>
2017-08-25 11:38:21 +0800

    增加歌曲页暂停和播放以及停止disc动画功能

--- song.js
+++ song.js
@@ -33,9 +33,28 @@ $(function () {
     })
 
     var audio = document.createElement('audio');
+    var $pause = $('.icon-pause');
+    var $play = $('.icon-play');
+    var $light = $('.disc-container .disc .light');
+    var $cover = $('.disc-container .disc .cover');
     audio.src = 'http://dl.stream.qqmusic.qq.com/C4000026paSX1I8n5H.m4a?vkey=1698A2779F2B414B1254EA92E49BE06DEC3B9365C3B3000CC3B825DF93FB10ABA7575FB373BBF389C20C43DF5A501A99C5FDEB80994B3443&guid=6038429688&uin=0&fromtag=66';
     audio.oncanplay = function() {
         audio.play();
         $('.disc').addClass('active');
+        $pause.css('display','block');
     }
+    $pause.on('click',function() {
+        audio.pause();
+        $pause.css('display','none');
+        $play.css('display','block');
+        $light.addClass('stop');
+        $cover.addClass('stop');
+    })
+    $play.on('click',function() {
+        audio.play();
+        $play.css('display','none');
+        $pause.css('display','block');
+        $light.removeClass('stop');
+        $cover.removeClass('stop');
+    })
 })
