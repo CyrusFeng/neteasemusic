e543084ba32596357f5d8da923ba0542da31d941
Elemento <18733660998@163.com>
2017-08-27 00:00:22 +0800

    完成热歌榜页面

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(function () {
     $.get("/songs.json").then(function (response) {
-         
-        response.forEach(function(i) {
+
+        response.forEach(function (i) {
             let $li = $(`
             <li>
                 <a href="song.html?id=${i.id}">
@@ -24,4 +24,50 @@ $(function () {
         $('.loadinggif').remove();
     })
 
+    var $indexNav = $('.indexNav');
+    var $tablist = $('.show>li');
+
+    $indexNav.on('click', '.tab li', function (e) {
+        var $li = $(e.currentTarget).addClass('active');
+        $li.siblings().removeClass('active');
+        var index = $li.index();
+
+        $li.trigger('tabchange', index);
+        $tablist.eq(index).addClass('actived').siblings().removeClass('actived');
+    })
+
+    $indexNav.on('tabchange', function (e, index) {
+        if ($tablist.eq(index).attr('data-downloaded') === 'yes') {
+            return;
+        }
+        if (index === 1) {
+            $.get("/songs.json").then(function (response) {
+                response.forEach(function (i) {
+                    let $li = $(`
+            <li>
+                <a href="song.html?id=${i.id}">
+                    <h3>${i.name}</h3>
+                    <p>
+                        <svg class="sq" aria-hidden="true">
+                            <use xlink:href="#icon-sq"></use>
+                        </svg>
+                        Taylor Swift - Look What You Made Me Do
+                    </p>
+                
+                    <svg class="play" aria-hidden="true">
+                        <use xlink:href="#icon-play"></use>
+                    </svg>
+                </a>        
+            </li>
+            `);
+                    $('.hotmusic ol').append($li);
+                }, this);
+                $('.loadinggif').remove();
+
+                 $tablist.eq(index).attr('data-downloaded','yes');
+            })
+        } else if(index === 1){
+
+        }
+    })
 })
