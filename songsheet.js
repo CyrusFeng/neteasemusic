baef15a3aee6bc5ab677bdb17a56889d59853724
Elemento <18733660998@163.com>
2017-08-28 16:09:19 +0800

    增加歌单简介展开和折叠功能

--- songsheet.js
+++ songsheet.js
@@ -1,7 +1,7 @@
 $(function () {
     $.get("/songsheet.json").then(function (response) {
-                response.forEach(function (item,i) {
-                    let $li = $(`
+        response.forEach(function (item, i) {
+            let $li = $(`
             <li>
             <a href="song.html?id=${item.id}">
                 <div class="number">${i+1}</div>
@@ -23,8 +23,38 @@ $(function () {
                 </a>        
             </li>
             `);
-                    $('.list ol').append($li);
-                }, this);
-                // $('.loadinggif').remove();
-            })
+            $('.list ol').append($li);
+        }, this);
+        // $('.loadinggif').remove();
+    })
+    var $introtext = $('main .intro .intro_text');
+    var $toggle = $('main .intro .intro_text .toggle');
+    var $down = $('main .intro .intro_text .toggle .icondown');
+    var $up = $('main .intro .intro_text .toggle .iconup');
+    
+    var $upnode = $(`
+                <svg class="iconup" aria-hidden="true">
+                      <use xlink:href="#icon-packup"></use>
+                </svg>
+            `);
+    var $downnode = $(`
+                <svg class="icondown" aria-hidden="true">
+                      <use xlink:href="#icon-unfold"></use>
+                </svg>
+            `);
+    $toggle.on('click',function() {
+        if ($down.css('display') !== 'none') {
+            $introtext.css('display', 'block').css('height', 'inherit');
+            $down.css('display','none');
+            $up.css('display','block');
+            return;
+        }
+        if($up.css('display') !== 'none'){
+            $introtext.css('display', '-webkit-box').css('height', '57px');
+            $up.css('display','none');
+            $down.css('display','block');
+            return;
+        }
+    })
+    
 })
